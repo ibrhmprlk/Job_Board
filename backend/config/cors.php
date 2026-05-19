4dfd825cdb4e14ca99592fe07470713010948e78
İbrahim Parlak <ibrahimparlak819@gmail.com>
2026-05-19 16:25:02 +0300

    Update cors.php

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -2,7 +2,10 @@
 return [
     'paths' => ['api/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:3000'],
+    'allowed_origins' => [
+        'http://localhost:3000',
+        'https://jobboard-ip.netlify.app',
+    ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
